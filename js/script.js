ba546dfb4403609fe2b0d9b345793f1663c04fca
Oksana Gochmen <43575123+Elif07@users.noreply.github.com>
2022-11-27 23:10:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// "use strict";
+
+// console.log('я вчу JS');
+// // домашка № 1
+
+// //варіант 1
+// console.log('я вчу JS')
+
+// //варіант 2
+// console.log('JS')
+// console.log('вчу JS')
+
+// //варіант 3
+// console.log('вчу') console.log('JS')
+
+// //варіант 4
+// console.log('я вчу JS);
+// console.log('я вчу JS');
+/*===============================================*/
+// домашка № 2
+// let eyesColor;
+// let user;
+// let userName = 'Вася';
+// user = userName;
+// console.log(user);
+
+//варіант 1
+// let user = 'Вася' let age = 36
+//варіант 2
+// let myage = 36;
+//варіант 3
+//  
+//варіант 4
+// const BLOCK_HEIGHT = 100;
+//варіант 5
+// if (true) {
+//    var size = 15;
+// }
+// console.log(size);
+
+/*==============================================*/
+// домашка № 3
+//варіант 1
+// let userAge = 36;
+// let userInfo = "Фрілансер ${userAge}";
+//варіант 2
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+//варіант 3
+// let userName;
+// console.log(typeof userName);
+//варіант 4
+// let userSize = '45' / '8';
+// console.log(typeof userSize);
